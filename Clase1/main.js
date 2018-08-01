bfa89cf4436f2baa6a8c9ea5cb6f3e593193170b
Moises Rivas <mardemza@hotmail.com>
2018-08-01 17:24:44 -0300

    Clase 1 - Primer sitio de prueba

---
+++ Clase1/main.js
@@ -0,0 +1,8 @@
+// Muestro recuadro de alerta con un recuadro para que ingrese su Nombre
+var msg = prompt("Escriba su nombre");
+
+// Salida por cartel del mismo navegador cliente.
+alert("Hola " + msg);
+
+// Salida por la consola del navegador Cliente.
+console.log("Hola " + msg);
